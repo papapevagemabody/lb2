64d72d21d9f38e5dbfd68f56f5804f0a618d9d41
papapevagemabody <146659187+papapevagemabody@users.noreply.github.com>
2023-10-02 10:44:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ app=FastAPI()
 @app.get("/calculator/")
 def calculator(first_number: str, action: str, second_number: str):
     if action=='+':
-        return {int(first_number) + int(second_number)}
+        return {'ответ':int(first_number) + int(second_number)}
     if action=='-':
-        return {int(first_number) - int(second_number)}
+        return {'ответ':int(first_number) - int(second_number)}
     if action=='*':
-        return {int(first_number) * int(second_number)}
-    if action=='/':
-        return {int(first_number) / int(second_number)}
+        return {'ответ':int(first_number) * int(second_number)}
+    if action=='/' and second_number !='0':
+        return {'ответ':int(first_number) / int(second_number)}
     if action=='**':
-        return {int(first_number)**int(second_number)}
-
+        return {'ответ':int(first_number)**int(second_number)}
+    if second_number=='0' and action=='/':
+        return {'невозможная операция'}
 
 
 
